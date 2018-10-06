631e7860a68399e35301bd5c553e2f7ba46fb000
paulhulbert <pxh8242@rit.edu>
2018-10-06 14:26:35 -0400

    Add boilderplat

---
+++ Application.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/build', methods=['GET'])
+def build():
+    return "hi"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
