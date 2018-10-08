850a6271b92cde8319cf65f803e72bb48f4e514f
paulhulbert <pxh8242@rit.edu>
2018-10-08 13:11:50 -0400

    Add branch deploy

--- Application.py
+++ Application.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from git import Git
 import os
 
@@ -8,23 +8,36 @@ g = Git('/home/pxh8242/supply-chain-visibility')
 
 @application.route('/mobileapi', methods=['GET'])
 def build_mobile_api():
-    os.system("bash PullAndDeploy.sh azureMobileApi master")
-    return "Mobile Api was deployed"
+    branch = request.args.get('branch', default='master')
+    return build_and_deploy("azureMobileApi", branch)
 
 @application.route('/dataadapter', methods=['GET'])
 def build_data_adapter():
-    os.system("bash PullAndDeploy.sh azureDataAdapter master")
-    return "Data Adapter was deployed"
+    branch = request.args.get('branch', default='master')
+    return build_and_deploy("azureDataAdapter", branch)
 
 
 @application.route('/branches', methods=['GET'])
+def get_list_of_branches_api():
+    return jsonify(get_list_of_branches())
+
+
+def build_and_deploy(project, branch):
+    g.pull()
+    if branch not in get_list_of_branches():
+        return "Branch" + branch + " is not in the list of branches"
+    os.system("bash PullAndDeploy.sh " + project + " " + branch)
+    return project + " was deployed"
+
+
 def get_list_of_branches():
     output = g.branch()
     branches_raw = output.split('\n')
     branches = []
     for branch in branches_raw:
         branches.append(branch.strip('*').strip())
-    return jsonify(branches)
+    return branches
+
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
