df01a76b273b80c456b1b8d97fc1d5640ac1123d
paulhulbert <pxh8242@rit.edu>
2018-10-08 12:46:01 -0400

    Add git branch reader

--- Application.py
+++ Application.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from git import Git
 import os
 
 application = Flask(__name__)
+
+g = Git('/home/pxh8242/supply-chain-visibility')
 
 @application.route('/mobileapi', methods=['GET'])
 def build_mobile_api():
@@ -13,6 +16,15 @@ def build_data_adapter():
     os.system("bash PullAndDeploy.sh azureDataAdapter master")
     return "Data Adapter was deployed"
 
+
+@application.route('/branches', methods=['GET'])
+def get_list_of_branches():
+    output = g.branch()
+    branches_raw = output.split('\n')
+    branches = []
+    for branch in branches_raw:
+        branches.append(branch.strip('*').strip())
+    return branches
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
